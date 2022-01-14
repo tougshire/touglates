674d20a7cd6b643e8d952c0a5780ec82bfcce55d
tougshire <tougshire@tougshire.com>
2022-01-14 16:50:26 -0500

    Updated functions

--- static/touglates/touglates.js
+++ static/touglates/touglates.js
@@ -5,22 +5,6 @@ var ctrl_opener = document.getElementById('input_opener')
     }
 }
 
-function addRelatedPopupButton( selectId, modelName, popupUrl ) {
-  var select = document.getElementById(selectId)
-  if( select != null ) {
-    button = document.createElement('button')
-    button.type = 'button'
-    button.id = 'btn_related_' + selectId
-    button.appendChild(document.createTextNode('add'))
-    button.addEventListener('click', function() {
-      window.open( popupUrl )
-      select.setAttribute('updateFrom' + modelName, 'True')
-    });
-    select.parentNode.insertBefore(button, select.nextSibling)
-
-  }
-}
-
 function addFilterInput( selectId ) {
   var select = document.getElementById(selectId)
   if( select != null ) {
@@ -59,25 +43,6 @@ function addFilterInput( selectId ) {
   }
 }
 
-function refreshFrom(optionValue, optionLabel, modelName, attrs=[]) {
-    let controlIds = getControlIds(modelName)
-    for( controlId of controlIds ) {
-      let control = document.getElementById(controlId)
-      let newOption = document.createElement('option')
-      newOption.value = optionValue
-      newOption.appendChild(document.createTextNode(optionLabel))
-      for(attr of attrs){
-          newOption.setAttribute(attr.name, attr.value)
-      }
-      if( control.getAttribute('updateFrom' + modelName) > "" ) {
-        newOption.setAttribute('selected', 'SELECTED')
-        control.removeAttribute('updateFrom' + modelName)
-      }
-      control.appendChild(newOption)
-      control.dispatchEvent(new Event('change'))
-    }
-}
-
 function addFormsetListener(buttonId, formsetPrefix, emptyDivId, insertPointId ) {
     document.getElementById(buttonId).addEventListener('click', function(e){
         e.preventDefault();
@@ -109,3 +74,139 @@ function addFormsetListener(buttonId, formsetPrefix, emptyDivId, insertPointId )
         }
     });
 }
+
+
+function addFormsetFormAfterPopup(fields, model, insertNearId='', after=false, delNear=false) {
+    var emptyDisplayDiv = document.getElementById('div_display-' + model + '-empty')
+    var emptyFormsetDiv = document.getElementById('div_formset-' + model + '-empty')
+    var newdiv = emptydiv.cloneNode(true)
+    var allDivsInList = document.getElementsByClassName('related-' + model)
+    newdiv.id = 'div_' + model + '-' + allDivsInList.length
+    keys = Object.keys(fields)
+    for(key of keys){
+        console.log(key)
+        let elements = newdiv.querySelectorAll("span[data-field='" + key + "']")
+        for( element of elements ){
+            let field = fields[key]
+            if(field['tag']) {
+            let innerEl = document.createElement(field['tag'])
+            let attrkeys = Object.keys(field['attrs'])
+            for( attrkey of attrkeys) {
+                innerEl.setAttribute(attrkey, field['attrs'][attrkey])
+            }
+            innerEl.innerText = field['text']
+            element.appendChild(innerEl)
+            } else {
+            console.log('replacing text')
+            element.innerText=field
+            }
+        }
+    }
+    newdiv.style.display="block"
+    let insertnear
+    if(insertNearId>''){
+        insertnear = document.getElementById(insertNearId)
+    } else {
+        insertnear = emptydiv
+    }
+    if(after){
+        insertnear.parentNode.insertBefore(newdiv, insertnear.nextSibling)
+    } else {
+        insertnear.parentNode.insertBefore(newdiv, insertnear)
+    }
+    if(delNear) {
+        insertnear.remove()
+    }
+}
+
+
+
+
+
+
+/**
+ * Refreshes a list of related objects after a popup is called.
+ *
+ * This function is called from the popped up window ex: window.opener.addDivAfterPopup({'key1':'value1'}, 'model').
+ * This function expects the calling window has a list of related object divs each of which has a class name 'related-' followed by the model name
+ * This function expects an empty div which this function will use as a template.  That div's id is 'div_' followed by the model name followed by '-empty'
+ * The empty div will contain a data element for each field to be copied into the div.  Those elements have attributes named 'data-field-' followed by the field name
+ *
+ * ex: <div id='div_model-empty'><span data-field-key1></span></div>
+ *
+ * @param {object} fields              Key value pairs for fields
+ * @param {string} model               Name of related model
+ * @param {string} [insertBeforeId=''] The ID of the element before which the new div is to be inserted.
+ *                                     if blank, the function will use the empty div
+ * @param {bool}   [delinbefore=false] If the element before which the new div is to be inserted is to be deleted
+ *
+ */
+
+function addDivAfterPopup(fields, model, insertNearId='', after=false, delNear=false) {
+    var emptydiv = document.getElementById('div_' + model + '-empty')
+    var newdiv = emptydiv.cloneNode(true)
+    var allDivsInList = document.getElementsByClassName('related-' + model)
+    newdiv.id = 'div_' + model + '-' + allDivsInList.length
+    keys = Object.keys(fields)
+    for(key of keys){
+        console.log(key)
+        let elements = newdiv.querySelectorAll("span[data-field='" + key + "']")
+        for( element of elements ){
+            let field = fields[key]
+            if(field['tag']) {
+            let innerEl = document.createElement(field['tag'])
+            let attrkeys = Object.keys(field['attrs'])
+            for( attrkey of attrkeys) {
+                innerEl.setAttribute(attrkey, field['attrs'][attrkey])
+            }
+            innerEl.innerText = field['text']
+            element.appendChild(innerEl)
+            } else {
+            console.log('replacing text')
+            element.innerText=field
+            }
+        }
+    }
+    newdiv.style.display="block"
+    let insertnear
+    if(insertNearId>''){
+        insertnear = document.getElementById(insertNearId)
+    } else {
+        insertnear = emptydiv
+    }
+    if(after){
+        insertnear.parentNode.insertBefore(newdiv, insertnear.nextSibling)
+    } else {
+        insertnear.parentNode.insertBefore(newdiv, insertnear)
+    }
+    if(delNear) {
+        insertnear.remove()
+    }
+}
+
+/**
+ * Adds to an element an event listener which opens a popup to edit model related to the object.
+ *
+ * This function expects an element such as a
+ * This function expects the calling window has a list of related object divs each of which has a class name 'related-' followed by the model name
+ * This function expects an empty div which this function will use as a template.  That div's id is 'div_' followed by the model name followed by '-empty'
+ * The empty div will contain a data element for each field to be copied into the div.  Those elements have attributes named 'data-field-' followed by the field name
+ *
+ * ex: <div id='div_model-empty'><span data-field-key1></span></div>
+ *
+ * @param {object} fields              Key value pairs for fields
+ * @param {string} model               Name of related model
+ * @param {string} [insertBeforeId=''] The ID of the element before which the new div is to be inserted.
+ *                                     if blank, the function will use the empty div
+ * @param {bool}   [delinbefore=false] If the element before which the new div is to be inserted is to be deleted
+ *
+ */
+function addAddRelatedPopupEvent(buttonId) {
+    addButton = document.getElementById('buttonId') /*doesn't have to be a button */
+    if(typeof(addButton) != 'undefined' && addButton != null){
+      addButton.addEventListener('click', function(e) {
+        e.preventDefault()
+        window.open(e.target.href)
+      });
+    }
+  }
