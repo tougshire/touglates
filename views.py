c2b7a1dcbfafef832df09ef6f5fd093096f4cce5
tougshire <tougshire@tougshire.com>
2023-09-04 08:41:13 -0400

    Add window_closer view

--- views.py
+++ views.py
@@ -1,3 +1,18 @@
+from django.apps import apps
+from django.http import HttpResponse
 from django.shortcuts import render
 
+def window_closer( request, app_name, model_name, pk ):
+
+    object = apps.get_model(app_name, model_name).objects.get(pk=pk)
+    label = str(object)
+
+    response_text = """
+    <script>
+        window.opener.addOptionFromPopup("{}","{}","{}")
+        window.close()
+    </script>
+    """.format(pk, label, model_name)
+
+    return HttpResponse(response_text)
 
