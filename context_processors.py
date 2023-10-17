855bc71f07309e7c6817f8247ccae232669e4732
tougshire <tougshire@tougshire.com>
2023-10-17 16:03:48 -0400

    Fix error when the app name is not touglates

--- context_processors.py
+++ context_processors.py
@@ -2,7 +2,11 @@ from django.conf import settings
 
 
 def touglates(request):
-    touglates = settings.TOUGLATES[request.resolver_match.app_name]
+    app_name = request.resolver_match.app_name
+    touglates = {}
+    if app_name in settings.TOUGLATES:
+        touglates = settings.TOUGLATES[app_name]
+
     if "SITE_TITLE" in settings.TOUGLATES and not "SITE_TITLE" in touglates:
         touglates["SITE_TITLE"] = settings.TOUGLATES["SITE_TITLE"]
     return {"touglates": touglates}
