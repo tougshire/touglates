86d3b05b3f574092007d86eeaeab447ca78cd480
tougshire <tougshire@tougshire.com>
2023-10-19 05:53:31 -0400

    Add key overwrite in context processor

--- context_processors.py
+++ context_processors.py
@@ -4,10 +4,10 @@ from django.conf import settings
 def touglates(request):
     app_name = request.resolver_match.app_name
     touglates = {}
+    if hasattr(settings, "TOUGLATES"):
+        touglates = settings.TOUGLATES
     if app_name in settings.TOUGLATES:
-        touglates = settings.TOUGLATES[app_name]
-
-    if "SITE_TITLE" in settings.TOUGLATES and not "SITE_TITLE" in touglates:
-        touglates["SITE_TITLE"] = settings.TOUGLATES["SITE_TITLE"]
+        for key in settings.TOUGLATES[app_name]:
+            touglates[key] = settings.TOUGLATES[app_name][key]
 
     return {"touglates": touglates}
