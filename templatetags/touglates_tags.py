00b9625914d6778870cbf14e20ae6b3dbf9b1478
tougshire <tougshire@tougshire.com>
2024-03-07 08:51:38 -0500

    Add html_sanitizer dependancy and render_with_format function

--- templatetags/touglates_tags.py
+++ templatetags/touglates_tags.py
@@ -3,28 +3,46 @@ from django import template
 from django.conf import settings
 from django.template.defaultfilters import stringfilter
 from django.urls import reverse
+from html_sanitizer.django import get_sanitizer
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.simple_tag
-def project_include_file( name='' ):
-    if name=='':
+def project_include_file(name=""):
+    if name == "":
         try:
             return settings.PROJECT_INCLUDE_FILE
         except AttributeError:
             return None
     else:
         try:
-            return settings.PROJECT_INCLUDE_FILES[ name ]
+            return settings.PROJECT_INCLUDE_FILES[name]
         except AttributeError:
             return None
 
+
 @register.filter
 def remove_linebreaks(value):
-    return( str(value).replace("\n", " ").replace("\r", " "))
+    return str(value).replace("\n", " ").replace("\r", " ")
 
 
 @register.filter()
 @stringfilter
 def markdown(value):
-    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+    return markdown.markdown(value, extensions=["markdown.extensions.fenced_code"])
+
+
+@register.filter
+@stringfilter
+def render_with_format(value, format="markdown"):
+    sanitizer = get_sanitizer(name="sdc_site")
+    if format == "markdown":
+        return sanitizer.sanitize(
+            md.markdown(value, extensions=["markdown.extensions.fenced_code"])
+        )
+    elif format == "html":
+        return sanitizer.sanitize(value)
+    elif format == "plaintext":
+        return sanitizer.sanitize(value.replace("\n", "<br/>"))
