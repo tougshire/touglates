faa629a01f0d48fc5e380231b44efc714395c8d9
tougshire <tougshire@tougshire.com>
2024-04-09 07:17:09 -0400

    Change 'add' to 'new' in default addLabel

--- static/touglates/touglates.js
+++ static/touglates/touglates.js
@@ -107,7 +107,7 @@ function addOptionFromRelatedPopup(optionValue, optionLabel, modelName, attrs=[]
 //     }
 // }
 
-function addRelatedPopupButton( selectId, modelName, addUrl, addLabel='add' ) {
+function addRelatedPopupButton( selectId, modelName, addUrl, addLabel='new' ) {
     var select = document.getElementById(selectId)
     if( select != null ) {
         select.dataset.model=modelName
