528e5e01e0f592c1a12dc43ef121def8abc7a551
tougshire <tougshire@tougshire.com>
2024-05-27 19:16:59 -0400

    Escape the object str  in the script

--- views.py
+++ views.py
@@ -1,11 +1,12 @@
 from django.apps import apps
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.html import escape
 
 
 def popup_closer(request, app_name, model_name, pk):
     object = apps.get_model(app_name, model_name).objects.get(pk=pk)
-    label = str(object)
+    label = escape(str(object))
 
     response_text = f"""
     <script>
