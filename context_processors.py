78fd2c3c60cadeba45e4071a531b7f302b6108c2
tougshire <tougshire@toughshire.com>
2025-01-25 05:58:35 -0500

    Add try-except block to context_processors for app_name

--- context_processors.py
+++ context_processors.py
@@ -2,14 +2,19 @@ from django.conf import settings
 
 
 def base_context_settings(request):
-    app_name = request.resolver_match.app_name
+    try:
+        app_name = request.resolver_match.app_name
+    except:
+        app_name = ""
     base_context_settings = {}
     if hasattr(settings, "BASE_CONTEXT_SETTINGS"):
         base_context_settings = settings.BASE_CONTEXT_SETTINGS
         if app_name in settings.BASE_CONTEXT_SETTINGS:
-            base_context_settings.update(settings.BASE_CONTEXT_SETTINGS[app_name])
+            if app_name > "":
+                base_context_settings.update(settings.BASE_CONTEXT_SETTINGS[app_name])
 
     return {"base_context_settings": base_context_settings}
+
 
 #depreciated in favor of base_context_settings above
 def touglates(request):
